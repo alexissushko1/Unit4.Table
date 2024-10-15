60f84729d46180422e02e9a2c1210398e12b6448
Alexis Sushko <alexis.sushko@gmail.com>
2024-10-15 13:14:22 -0400

    Updated schema and seed code

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const prisma = require("./prisma");
 
 app.use(require("morgan")("dev"));
 app.use(express.json());
 
 // TODO: POST /reservations
+
+app.post("/reservations", async (req, res, next) => {
+  try {
+    const { date, restaurantId, customerIds } = req.body;
+    const party = customerIds.map((id) => ({ id: +id }));
+    const reservation = await prisma.reservation.create({
+      data: {
+        date,
+        restaurantId: +restaurantId,
+        party: { connect: party },
+      },
+    });
+    res.status(201).json(reservation);
+  } catch (e) {
+    next(e);
+  }
+});
 
 app.use((req, res, next) => {
   next({ status: 404, message: "Endpoint not found." });
